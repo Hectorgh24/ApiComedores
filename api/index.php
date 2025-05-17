5bb1d7a7e2bba1891faad7528413a45315768b12
Hectorgh24 <hectorggh24@gmail.com>
2025-05-16 18:01:23 -0600

    correcion de index.php 2

--- api/index.php
+++ api/index.php
@@ -6,13 +6,14 @@ require_once __DIR__ . '/../src/Comentarios/routes/ComentariosRoutes.php';
 $request_uri = $_SERVER['REQUEST_URI'];
 $request_method = $_SERVER['REQUEST_METHOD'];
 
+/*
 if (strpos($request_uri, '/api/comedores/') === 0) {
     // La ruta ya será manejada por empleadoRoutes.php
     // que incluimos arriba
 
 }
-/* else {
-    header("HTTP/1.1 404 Not Found");
-    echo "<error>Ruta no encontrada</error>";
-}*/
+    */
+
+header("HTTP/1.1 404 Not Found");
+echo "<error>Ruta no encontrada</error>";
 ?>
