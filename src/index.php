35f1d0e092c905d725d6b0318c322d43afa9f645
VictorMongeM <victor.monge.morales@gmail.com>
2025-05-22 21:19:06 -0600

    CORS

--- src/index.php
+++ src/index.php
@@ -1,4 +1,7 @@
 <?php
+require_once __DIR__ . '/../middleware/Cors.php';
+Cors::permitirOrigen();
+
 require_once __DIR__ . '/DesayunosComidasCarta/routes/DesayunoComidaRoutes.php';
 require_once __DIR__ . '../../src/ProductosCarta/routes/ProductoCartaRoutes.php';
 require_once __DIR__ . '../src/Comentarios/routes/ComentariosRoutes.php';
